ce31435db030ae429e9a109dfbc3340c1b2990a6
giftontoolkit <giftontoolkit@gmail.com>
2025-02-27 21:05:57 +0500

    index.php

--- v1/index.php
+++ v1/index.php
@@ -4,12 +4,14 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-    <title><?php bloginfo('name'); ?></title>
+    <title><?php bloginfo('name'); ?> - Landing Page</title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 
     <h1>This is heading in index.php</h1>
-
+    <p>We got blogname by using <b>bloginfo</b> function. This blog name is <span style="color: red;"> <?php bloginfo('name')?> </span>This is set by using</p>
+    <p>Styling sheet is placed in below link</p>
+    <?php echo get_stylesheet_uri(); ?>
 </body>
 </html>
