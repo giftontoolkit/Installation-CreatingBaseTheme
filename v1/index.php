5bd47ba1624679b969ce6221230f8a11a113788f
giftontoolkit <giftontoolkit@gmail.com>
2025-02-27 20:44:52 +0500

    Add files via upload

---
+++ v1/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+    <h1>This is heading in index.php</h1>
+
+</body>
+</html>
